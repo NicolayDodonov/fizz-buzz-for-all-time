a52914cdb97ff26d2262b66370c8643bfdb0306d
NicolayDodonov <95965202+NicolayDodonov@users.noreply.github.com>
2024-11-01 14:23:46 +0400

    add Pascal ABC realisation

---
+++ pascal/main.pas
@@ -0,0 +1,22 @@
+﻿program main;
+  var 
+    s: string;
+    i: integer;
+    
+function FizzBuzz(number: integer): string;
+var
+  res: string;
+begin
+  if number mod 3 = 0 then
+    res := res + 'Fizz';
+  if number mod 5 = 0 then
+    res := res + 'Buzz';
+  if res = '' then
+    res := IntToStr(number);
+  FizzBuzz := res
+end;  
+
+begin
+  for i:= 1 to 100 do 
+    write(FizzBuzz(i)+' ');
+end.
